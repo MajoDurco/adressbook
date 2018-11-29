5271cb3951a78e3fdf6c2c45c667f896218ddf98
Majo Durco <majodurco@gmail.com>
2018-11-29 07:31:21 +0100

    Add nodemon for development

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require('./src/routes')
 const app = express()
 
 app.use(express.json())
+app.use('/', routes.rootRouter)
 app.use('/login', routes.login)
 app.use('/contacts', routes.contacts)
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,16 @@
+const express = require('express')
+
 const contacts = require('./contacts')
 const login = require('./login')
 
+const rootRouter = express.Router()
+
+rootRouter.get('/', (req, res) => {
+  res.send('Home')
+})
+
 module.exports = {
+  rootRouter,
   contacts,
   login,
 }
