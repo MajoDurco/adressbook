08c4555ad7c19bdcf1958826441d79454c8b8a4f
Majo Durco <majodurco@gmail.com>
2018-12-02 17:43:18 +0100

    User .env for secrets

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
-
-const env = process.env.ENV || 'local';
-
 const config = {
-  isLocalEnv() { return env === 'local'},
-  isProductionEnv() { return env === 'production' },
   jwtExpiration: process.env.JWT_EXPIRATION || '1h',
   jwtSecret: process.env.JWT_SECRET || 'wzBPT8J9hCTshGgvLkeKWd9c',
   port: process.env.PORT || 8080,
+  firebase: {
+    apiKey: process.env.FIREBASE_APIKEY,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  },
+  mongo: {
+    atlasUrl: process.env.MONGO_ATLAS_URL
+  }
 }
 
 module.exports = config
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,16 +1,9 @@
 const firebase = require('firebase/app')
 require('firebase/firestore')
 
-const config = {
-  apiKey: "AIzaSyAQqc0FnOtFa_LQP2jZX7aJCasphnQ1sqc",
-  authDomain: "adressbook-712e8.firebaseapp.com",
-  databaseURL: "https://adressbook-712e8.firebaseio.com",
-  projectId: "adressbook-712e8",
-  storageBucket: "adressbook-712e8.appspot.com",
-  messagingSenderId: "529018047906"
-};
+const config = require('../../config')
 
-firebase.initializeApp(config);
+firebase.initializeApp(config.firebase);
 
 // Set this options to remove warning about
 // Date object which is going to be changed in firestore
