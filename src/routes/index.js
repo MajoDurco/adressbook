7b629a956677be6a69cc723416d8b13a45120951
Majo Durco <majodurco@gmail.com>
2018-12-02 17:53:19 +0100

    User welcome message for root get

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,29 +2,23 @@ const express = require('express')
 
 const contacts = require('./contacts')
 const login = require('./login')
-const { getAdressBookDB } = require('../utils/getDatabase')
+const httpCodes = require('../constants/httpCodes')
 
 const rootRouter = express.Router()
 
 // eslint-disable-next-line
 rootRouter.get('/', async (_, res) => {
   try {
-    const db = await getAdressBookDB()
-    const collection = db.collection('users')
-    console.log(await collection.find().toArray())
-    res.send('Home')
+    res.send(`
+      This application was developed by Marian Durco.
+      If you want to know more about it visit the github repository:
+      https://github.com/MajoDurco/adressbook
+    `)
   } catch (err) {
     console.log(err)
-    res.send('err')
+    res.status(httpCodes.INTERNAL_ERROR)
+    res.send('Internal server error!')
   }
-})
-
-// eslint-disable-next-line
-rootRouter.post('/', async (_, res) => {
-  const db = await getAdressBookDB()
-  const collection = db.collection('users')
-  await collection.remove({})
-  res.send('removed')
 })
 
 module.exports = {
